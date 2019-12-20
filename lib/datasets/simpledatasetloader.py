d14a8388212510fd8501e891f046c57786973ee5
ahsanul08 <ahsanul.haque.ovi@gmail.com>
2019-12-21 00:23:20 +0600

    Added datasetloader

---
+++ lib/datasets/simpledatasetloader.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import numpy as np
+
+
+class SimpleDatasetLoader:
+    def __init__(self, preprocessors=None):
+        self.preprocessors = preprocessors
+
+        if self.preprocessors is None:
+            self.preprocessors = []
+
+    def load(self, image_paths, verbose=-1):
+        data = []
+        labels = []
+
+        for (i, image_path) in enumerate(image_paths):
+            image = cv2.imread(image_path)
+            label = image_path.split(os.path.sep)[-2]
+
+            for p in self.preprocessors:
+                image = p.preprocess(image)
+
+            data.append(image)
+            labels.append(label)
+
+            # show update (as verbosity level) while processing image
+
+            if verbose > 0 and i > 0 and (i+1) % verbose == 0:
+                print("[INFO] processed {}/{}".format(i + 1, len(image_paths)))
+
+        return np.array(data), np.array(labels)
